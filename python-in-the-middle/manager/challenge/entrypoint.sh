14bc73138eba7383a283fb465b980f3a55264efc
Tobias Müller <git@tsmr.eu>
2022-04-16 21:29:16 +0200

    Update entrypoint.sh

--- python-in-the-middle/manager/challenge/entrypoint.sh
+++ python-in-the-middle/manager/challenge/entrypoint.sh
@@ -3,7 +3,7 @@
 service ssh start
 
 useradd -m -p $(openssl passwd -1 $ENV_SSH_PASSWORD) -s /bin/bash user
-echo 'user ALL=(root) NOPASSWD: /usr/sbin/iptables, /usr/sbin/tcpdump -i lo tcp and port 1337 -A' >> /etc/sudoers
+echo 'user ALL=(root) NOPASSWD: /usr/sbin/iptables, /usr/bin/tcpdump -i lo tcp -A' >> /etc/sudoers
 
 sudo -u ctf python3 /home/ctf/alice.py & \
     sudo -u ctf python3 /home/ctf/bob.py
